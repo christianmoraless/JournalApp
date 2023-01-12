c2e7f9a08d295fd98e045337a77d840619d3c97a
Christian Morales <christiandoraless@gmail.com>
2023-01-12 12:28:47 -0400

    feat:commit

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 import React from "react";
 import { NavBar, SideBar } from "../components";
 
@@ -10,6 +10,7 @@ export const JournalLayout = ({ children }) => {
       <NavBar drawerWith={drawerWith} />
       <SideBar drawerWith={drawerWith} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,12 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant="body1">
-        Anim aute elit aliqua do tempor ad elit cillum reprehenderit aliquip
-        pariatur eu quis. Nisi adipisicing sunt do aliqua voluptate aliquip
-        anim. Dolore magna exercitation ullamco ipsum exercitation cillum
-        proident id sunt deserunt.
-      </Typography>
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
