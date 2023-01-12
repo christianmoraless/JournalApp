c7e5ccd892914a8c8f59679ed82fbe29f48317d0
Christian Morales <christiandoraless@gmail.com>
2023-01-12 11:25:48 -0400

    feat:creating sidebar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
 
 const drawerWith = 240;
 
@@ -8,6 +8,7 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWith={drawerWith} />
+      <SideBar drawerWith={drawerWith} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {children}
       </Box>
