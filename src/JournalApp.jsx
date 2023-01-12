bcd285d9674780828add02ff978f55d331f7a53f
Christian Morales <christiandoraless@gmail.com>
2023-01-11 21:12:55 -0400

    first commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,10 @@
+import { AppRouter } from "./router/AppRouter";
+
+export const JournalApp = () => {
+  console.log("renderizo");
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+};
---
+++ src/journal/routes/JournalRoutes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { JournalApp } from "../../JournalApp";
+
+export const JournalRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<JournalApp />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
